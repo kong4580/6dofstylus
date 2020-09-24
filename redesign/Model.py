01e1f6e4f38548a1ed8d3ebf4c08129a4a7cc729
kong4580 <redkong4580@gmail.com>
2020-09-24 13:13:34 +0700

    add test

--- redesign/Model.py
+++ redesign/Model.py
@@ -74,7 +74,7 @@ class Model():
                                 [-1,0,0,0],
                                 [0,0,0,1]])
             transform = np.eye(4)
-            
+            "test"
 
         # if model is not cursor
         else:
