ef2b9ca3bd64d90af7b6d1f8f7e53a00882f63b5
nymph <nymph@yannix.com>
2020-09-25 14:07:46 +0700

    switch port

--- redesign/main.py
+++ redesign/main.py
@@ -105,8 +105,8 @@ def openGUI(samplingRate = 0.005):
 if __name__ == '__main__':
     
     # declare port
-    # port = '/dev/pts/0' # ubuntu port
-    port = '/dev/ttyUSB0' # arduino port
+    port = '/dev/pts/0' # ubuntu port
+    # port = '/dev/ttyUSB0' # arduino port
     
     # declare constants
     samplingRate = 0.005
