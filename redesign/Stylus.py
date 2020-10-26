755376a48d90716f011fd36d313a2be453559100
kong4580 <redkong4580@gmail.com>
2020-10-26 17:22:58 +0700

    update stylus controller

--- redesign/Stylus.py
+++ redesign/Stylus.py
@@ -130,9 +130,9 @@ class Stylus2(Stylus):
             q[1] = q[1] - pi 
             q[2] = q[2] - pi 
 
-            q = q - np.asarray([-0.06902914, -0.0475534,  -0.05368933])
+            q = q - np.asarray([0.0398835,  0.00306796, 0.02607767])
             q = np.append(q,[0])
-            # print(q)
+            print(q)
             fk = self.forwardKinematics(q.tolist(),wrt=(0,3))[-1,:,:]
             # print(fk)
             return fk
