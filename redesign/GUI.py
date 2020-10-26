e9f17bf2a7d0edd11fe4754de27c216988d03a82
nymph <nymph@yannix.com>
2020-10-26 13:41:46 +0700

    clean code

--- redesign/GUI.py
+++ redesign/GUI.py
@@ -20,16 +20,16 @@ class Gui():
     def __init__(self):
         self.controllerMode = None
         # init fltk window
-        self.__initWindow(size=(800,600),name="UI")
+        self.initWindow(size=(800,600),name="UI")
         
         # init opengl window class
-        self.__initOpenglWindow(size=(0,0,600,600),name="opengl")
+        self.initOpenglWindow(size=(0,0,600,600),name="opengl")
 
         #resize fltk and opengl window
         self.window.resizable(self.openglWindow)
         
         # init side output
-        self.__initOutputWidgetStorage()
+        self.initOutputWidgetStorage()
         
         # set config
         self.cfg={"homeCfg":(5.23747141, -0.01606842, -0.3270202)}
@@ -61,41 +61,132 @@ class Gui():
         
 
     # init fltk window
-    def __initWindow(self,size=(800,600),name="UI"):
+    def initWindow(self,size=(800,600),name="UI"):
         print("Init GUI window ... ",end="")
+        # set position x,position y, width, height, name
         self.window = fltk.Fl_Window(int(fltk.Fl.w()/2-size[0]/2),int(fltk.Fl.h()/2-size[1]/2),size[0],size[1],name)
         print("Done !")
+
     # init opengl window
-    def __initOpenglWindow(self,size=(0,0,600,600),name="opengl"):
+    def initOpenglWindow(self,size=(0,0,600,600),name="opengl"):
         print("Init openGl window ... ",end="")
+        # set position x,position y, width, height, name
         self.openglWindow = OpenGLWindow(size[0],size[1],size[2],size[3],name)
         print("Done !")
         
     # init side output
-    def __initOutputWidgetStorage(self):
-        # self.outputWidgetStorage = self.__createOutputWidget()
-        self.cameraSliderBarWidget()
-        self.cursorSpeedSliderBarWidget()
-        self.cameraInputWidget()
+    def initOutputWidgetStorage(self):
+        # model position output
         self.positionOutputWidget()
+
+        # camera widget
+        self.cameraSliderBarWidget()
+        self.cameraInputWidget()
+
+        # cursor speed widget
+        self.cursorSpeedSliderBarWidget()
+        
         # self.textWidget()
+
+        # test mode informing
         self.testModeUIOff()
         self.testModeUIOn()
+
+        # iou score output
+        self.iouOutput()
+
+        # coordinate informing
         self.coordinateOn()
         self.coordinateOff()
-        self.iouOutput()
-    
-    # text local coordinate
-    def coordinateOn(self):
-        testStr = "AXIS : LOCAL"
-        self.coordinateTextON = fltk.Fl_Box(682,570,100,25,testStr)
-        self.coordinateTextON.labelfont(fltk.FL_BOLD)
+        
+    # model position output        
+    def positionOutputWidget(self):
+        self.storageOutput = []
+        
+        # start position
+        yWidgetPosition = 0
 
-    # text world coordinate
-    def coordinateOff(self):
-        testStr = "AXIS : GLOBAL"
-        self.coordinateTextOff = fltk.Fl_Box(688,570,100,25,testStr)
-        self.coordinateTextOff.labelfont(fltk.FL_BOLD)
+        # init value
+        self.sliderBarValue = self.openglWindow.positionValue
+
+        # init label name
+        self.nameLabel = ["TransX", "TransY", "TransZ", "RotX", "RotY", "RotZ"]
+
+        for n in range(6) :
+
+            output = self.createOutputWidget(655,yWidgetPosition,145,25,self.nameLabel[n],self.sliderBarValue[n])
+            yWidgetPosition = yWidgetPosition + 25
+            self.storageOutput.append(output)
+
+    # camera sliderbar control widget
+    def cameraSliderBarWidget(self):
+        self.storageCamera = []
+        self.cameravalue= self.openglWindow.cameravalue
+        self.nameLabel = ["Zoom", "CameraX", "CameraY"]
+        minValue =       [ 0.01     , -1     , -1     ]
+        # minValue = [1]*6
+        maxValue =       [ 10       ,  1     ,  1     ]
+        stepValue = 0.001
+        yWidgetPosition = 150
+        for n in range(3) :
+            slider = self.createSliderBarWidget(695,yWidgetPosition,105,25,self.nameLabel[n],minValue[n],maxValue[n],self.cameravalue[n],stepValue)
+            yWidgetPosition = yWidgetPosition + 25
+            slider.callback(self.sliderCameraCB,n)
+            self.storageCamera.append(slider)
+
+    # bunch of input for controlling this UI
+    def cameraInputWidget(self):
+        self.storageInput = []
+        yWidgetPosition = 150
+        self.cameravalue = self.openglWindow.cameravalue
+        self.nameLabel = ["Zoom", "CamX", "CamY"]
+        for n in range(3) :
+            inp = self.createInputWidget(655,yWidgetPosition,40,25,self.nameLabel[n],self.cameravalue[n])
+            yWidgetPosition = yWidgetPosition + 25
+            inp.callback(self.inputCameraCB,n)
+            self.storageInput.append(inp)
+
+
+    # camera slider bar call back
+    def sliderCameraCB(self,widget, v):
+        widgetValue = widget.value()
+        self.cameravalue[v] = widgetValue
+        self.storageInput[v].value(str(widgetValue))
+    
+    # camera input call back
+    def inputCameraCB(self,widget, v):
+        widgetValue = float(widget.value())
+        widget.value(str(widgetValue))
+        self.cameravalue[v] = widgetValue
+
+    # cursor speed control widget
+    def cursorSpeedSliderBarWidget(self):
+        self.cursorSpeed = []
+        self.cursorSpeedValue = self.openglWindow.flags['cursorSpeed']
+        slider = self.createSliderBarWidget(695,225,105,25,"Speed",0.0,2.0,self.cursorSpeedValue,0.25)
+        slider.callback(self.sliderCursorSpeedCB,1)
+        inputw = self.createInputWidget(655,225,40,25,"Speed",self.cursorSpeedValue)  
+        inputw.callback(self.inputCursorSpeedCB,1)      
+        self.cursorSpeed.append(slider)
+        self.cursorSpeed.append(inputw)
+
+    def sliderCursorSpeedCB(self,widget,n):
+        widgetValue = widget.value()
+        self.cursorSpeed[0] = widgetValue
+        self.openglWindow.flags['cursorSpeed'] = widgetValue
+        self.cursorSpeed[1].value(str(widgetValue))
+        
+
+    def inputCursorSpeedCB(self,widget,n):
+        widgetValue = float(widget.value())
+        widget.value(str(widgetValue))
+        self.cursorSpeed[0] = widgetValue
+        self.openglWindow.flags['cursorSpeed'] = widgetValue
+
+    def cursorSpeedWidget(self):
+        if self.controllerMode == 'mouse':
+            self.cursorSpeed[0].hide()
+            self.cursorSpeed[1].hide()
 
     # text test mode off
     def testModeUIOff(self):
@@ -115,6 +206,15 @@ class Gui():
         c = fltk.fl_rgb_color(51, 204, 0)
         self.testTextOn.labelcolor(c)
 
+    # test mode text handle
+    def testModeUIHandle(self,testMode):
+        if testMode == True:
+            self.testTextOn.show()
+            self.testTextOff.hide()
+        elif testMode == False:
+            self.testTextOn.hide()
+            self.testTextOff.show()
+
     # create IoU output for all image
     def iouOutput(self):
         self.storageIouOutput = []
@@ -128,6 +228,31 @@ class Gui():
             output.value(str(0))
             y+=30
             self.storageIouOutput.append(output)
+
+    # text local coordinate
+    def coordinateOn(self):
+        testStr = "AXIS : LOCAL"
+        self.coordinateTextON = fltk.Fl_Box(682,570,100,25,testStr)
+        self.coordinateTextON.labelfont(fltk.FL_BOLD)
+
+    # text world coordinate
+    def coordinateOff(self):
+        testStr = "AXIS : GLOBAL"
+        self.coordinateTextOff = fltk.Fl_Box(688,570,100,25,testStr)
+        self.coordinateTextOff.labelfont(fltk.FL_BOLD)
+
+    # coordinate text handle
+    def coordinateUIHandle(self,mode):
+        if self.controllerMode == 'mouse':
+            if mode == True:
+                self.coordinateTextON.show()
+                self.coordinateTextOff.hide()
+            elif mode == False:
+                self.coordinateTextON.hide()
+                self.coordinateTextOff.show() 
+        else:
+            self.coordinateTextON.hide()
+            self.coordinateTextOff.hide()
     
     # create text widget
     def textWidget(self):
@@ -157,7 +282,7 @@ class Gui():
                    )
 
     # create slider bar widget
-    def __createSliderBarWidget(self,xSliderBarPosition,ySliderBarPosition,widthSliderBar,heightSliderBar,nameLabel,minValue,maxValue,beginValue,stepValue):
+    def createSliderBarWidget(self,xSliderBarPosition,ySliderBarPosition,widthSliderBar,heightSliderBar,nameLabel,minValue,maxValue,beginValue,stepValue):
             slider = fltk.Fl_Hor_Slider(xSliderBarPosition,ySliderBarPosition,widthSliderBar,heightSliderBar)
             slider.minimum(minValue)
             slider.maximum(maxValue)
@@ -167,7 +292,7 @@ class Gui():
             return slider
 
     # create input widget
-    def __createInputWidget(self,xInputPosition,yInputPosition,widthInput,heightInput,nameLabel,beginValue):
+    def createInputWidget(self,xInputPosition,yInputPosition,widthInput,heightInput,nameLabel,beginValue):
             inp = fltk.Fl_Input(xInputPosition,yInputPosition,widthInput,heightInput,nameLabel)
             inp.value(str(beginValue))
             inp.align(fltk.FL_ALIGN_LEFT)
@@ -175,119 +300,23 @@ class Gui():
             return inp
     
     # create output widget
-    def __createOutputWidget(self,xInputPosition,yInputPosition,widthInput,heightInput,nameLabel,beginValue):
+    def createOutputWidget(self,xInputPosition,yInputPosition,widthInput,heightInput,nameLabel,beginValue):
             out = fltk.Fl_Output(xInputPosition,yInputPosition,widthInput,heightInput,nameLabel)
             out.value(str(beginValue))
             out.align(fltk.FL_ALIGN_LEFT)
             return out
 
-    #bunch of slider bar for controlling this UI
-    def __allsliderBarWidget(self):
-        self.storageArea = []
-        # self.sliderBarValue =[0, 0, 0, 0, 0, 0, 0, 0, 0]
-        self.sliderBarValue = self.openglWindow.positionValue
-        self.nameLabel = ["TransX", "TransY", "TransZ", "RotX", "RotY", "RotZ"]
-        minValue =       [-10     , -10     , -10     , -180  , -180  , -180  ]
-        maxValue =       [ 10     ,  10     ,  10     ,  180  ,  180  ,  180  ]
-        stepValue = 0.1
-        yWidgetPosition = 0
-        for n in range(6) :
-            slider = self.__createSliderBarWidget(695,yWidgetPosition,105,25,self.nameLabel[n],minValue[n],maxValue[n],self.sliderBarValue[n],stepValue)
-            yWidgetPosition = yWidgetPosition + 25
-            slider.callback(self.__sliderCB,n)
-            self.storageArea.append(slider)
-    
-    # camera sliderbar control widget
-    def cameraSliderBarWidget(self):
-        self.storageCamera = []
-        self.cameravalue= self.openglWindow.cameravalue
-        self.nameLabel = ["Zoom", "CameraX", "CameraY"]
-        minValue =       [ 0.01     , -1     , -1     ]
-        # minValue = [1]*6
-        maxValue =       [ 10       ,  1     ,  1     ]
-        stepValue = 0.001
-        yWidgetPosition = 150
-        for n in range(3) :
-            slider = self.__createSliderBarWidget(695,yWidgetPosition,105,25,self.nameLabel[n],minValue[n],maxValue[n],self.cameravalue[n],stepValue)
-            yWidgetPosition = yWidgetPosition + 25
-            slider.callback(self.__sliderCameraCB,n)
-            self.storageCamera.append(slider)
-
-    def cursorSpeedSliderBarWidget(self):
-        self.cursorSpeed = []
-        self.cursorSpeedValue = self.openglWindow.flags['cursorSpeed']
-        slider = self.__createSliderBarWidget(695,225,105,25,"Speed",0.0,2.0,self.cursorSpeedValue,0.25)
-        slider.callback(self.slidercursorSpeedCB,1)
-        inputw = self.__createInputWidget(655,225,40,25,"Speed",self.cursorSpeedValue)  
-        inputw.callback(self.inputcursorSpeedCB,1)      
-        self.cursorSpeed.append(slider)
-        self.cursorSpeed.append(inputw)
-
-    def slidercursorSpeedCB(self,widget,n):
-        widgetValue = widget.value()
-        self.cursorSpeed[0] = widgetValue
-        self.openglWindow.flags['cursorSpeed'] = widgetValue
-        self.cursorSpeed[1].value(str(widgetValue))
-        
-
-    def inputcursorSpeedCB(self,widget,n):
-        widgetValue = float(widget.value())
-        widget.value(str(widgetValue))
-        self.cursorSpeed[0] = widgetValue
-        self.openglWindow.flags['cursorSpeed'] = widgetValue
-
-
-    # model position output        
-    def positionOutputWidget(self):
-        self.storageOutput = []
-        yWidgetPosition = 0
-        self.sliderBarValue = self.openglWindow.positionValue
-        self.nameLabel = ["TransX", "TransY", "TransZ", "RotX", "RotY", "RotZ"]
-        for n in range(6) :
-            output = self.__createOutputWidget(655,yWidgetPosition,145,25,self.nameLabel[n],self.sliderBarValue[n])
-            yWidgetPosition = yWidgetPosition + 25
-            self.storageOutput.append(output)
-        
-    # test mode text handle
-    def testModeUIHandle(self,testMode):
-        if testMode == True:
-            self.testTextOn.show()
-            self.testTextOff.hide()
-        elif testMode == False:
-            self.testTextOn.hide()
-            self.testTextOff.show()
-    # coordinate text handle
-    def coordinateUIHandle(self,mode):
-        if self.controllerMode == 'mouse':
-            if mode == True:
-                self.coordinateTextON.show()
-                self.coordinateTextOff.hide()
-            elif mode == False:
-                self.coordinateTextON.hide()
-                self.coordinateTextOff.show() 
-        else:
-            self.coordinateTextON.hide()
-            self.coordinateTextOff.hide()
-
-    def cursorSpeedWidget(self):
-        if self.controllerMode == 'mouse':
-            self.cursorSpeed[0].hide()
-            self.cursorSpeed[1].hide()
     # update fltk from opengl 
     def updateFltk(self):
         model = self.openglWindow.modelDicts['model'][self.openglWindow.modelDicts['runModelIdx']]
-        # print(model)
         r = R.from_matrix(model.worldToLocal[0:3,0:3])
         degree = r.as_euler('zyx', degrees=True)
         self.storageOutput[0].value(str(round(model.worldToLocal[0][3],2)))
         self.storageOutput[1].value(str(round(model.worldToLocal[1][3],2)))
         self.storageOutput[2].value(str(round(model.worldToLocal[2][3],2)))
-        # print(round(model.worldToLocal[2][3],2))
-        # print("*********************")
         self.storageOutput[3].value(str(round(degree[2],2)))
         self.storageOutput[4].value(str(round(degree[1],2)))
         self.storageOutput[5].value(str(round(degree[0],2)))
-        # print("what")
         self.iouScore = [0,0,0,0,0,0]
         self.iouScore[:len(self.openglWindow.iouScore)] = self.openglWindow.iouScore
         for i in range(len(self.iouScore)):
@@ -297,7 +326,7 @@ class Gui():
         self.testModeUIHandle(self.openglWindow.flags['lineupTestMode'])
         self.coordinateUIHandle(self.openglWindow.flags['coordinate'])
         self.cursorSpeedWidget()
-        print(self.window.x())
+
     # collect information from tester
     def loghandle(self):
         if self.addLog == True:
@@ -317,7 +346,6 @@ class Gui():
             self.openglWindow.flags['addLog'] = False
             self.addLog = False
         if self.openglWindow.flags['logFinish'] == True:
-            # print(self.openglWindow.log)
             self.log.update(self.openglWindow.log)
             with open(self.logFileName,"a+",newline='') as csvFile:
                 dictWriter = csv.DictWriter(csvFile,fieldnames = self.log.keys())
@@ -328,7 +356,7 @@ class Gui():
 
     # pop up score window after finish testing 
     def createScoreWindow(self):
-        self.scoreWindow = fltk.Fl_Window(400,100)
+        self.scoreWindow = fltk.Fl_Window(int(self.window.x() + self.window.w()/2-400/2),int(self.window.y()+self.window.h()/2-100/2),400,100)
         self.textbox = fltk.Fl_Box(100,10,200,25,"Finish Testing")
         self.textbox.box(fltk.FL_NO_BOX)
         self.textbox.labelsize(28)
@@ -348,7 +376,7 @@ class Gui():
     # create log window
     def createlogWindow(self):
         x = 150
-        self.logWindow = fltk.Fl_Window(500,200)
+        self.logWindow = fltk.Fl_Window(int(self.window.x() + self.window.w()/2-500/2),int(self.window.y()+self.window.h()/2-200/2),500,200)
         self.name = fltk.Fl_Input(x,10,300,25,"Name                  ")
         self.department = fltk.Fl_Input(x,40,300,25,"Department       ")
         self.Hand = fltk.Fl_Choice(x,70,300,25,"Dominant Hand")
@@ -379,30 +407,6 @@ class Gui():
         # print(self.log)
         self.createScoreWindow()
         
-    # bunch of input for controlling this UI
-    def cameraInputWidget(self):
-        self.storageInput = []
-        yWidgetPosition = 150
-        self.cameravalue = self.openglWindow.cameravalue
-        self.nameLabel = ["Zoom", "CamX", "CamY"]
-        for n in range(3) :
-            inp = self.__createInputWidget(655,yWidgetPosition,40,25,self.nameLabel[n],self.cameravalue[n])
-            yWidgetPosition = yWidgetPosition + 25
-            inp.callback(self.__inputCB,n)
-            self.storageInput.append(inp)
-
-
-    # camera slider bar call back
-    def __sliderCameraCB(self,widget, v):
-        widgetValue = widget.value()
-        self.cameravalue[v] = widgetValue
-        self.storageInput[v].value(str(widgetValue))
-    
-    # camera input call back
-    def __inputCB(self,widget, v):
-        widgetValue = float(widget.value())
-        widget.value(str(widgetValue))
-        self.cameravalue[v] = widgetValue
                 
     # main call back update ui
     def updateUI(self):
@@ -413,7 +417,7 @@ class Gui():
         self.openglWindow.redraw()
         self.updateFltk()
         # update side output value
-        self.__updateOutput(cvtedPose)
+        self.updateOutput(cvtedPose)
         
             
         return
@@ -466,7 +470,7 @@ class Gui():
     
     
     # update output function
-    def __updateOutput(self,pos):
+    def updateOutput(self,pos):
         for i in range(6):
             a=float("{:.2f}".format(pos[i]))
             self.outputWidgetStorage[i].value(str(a))
