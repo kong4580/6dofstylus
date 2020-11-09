431f2eec3769eebebb370f9ff5c293f0ed01c787
nymph <nymph@yannix.com>
2020-11-09 12:13:44 +0700

    wrong edit

--- redesign/OpenGLWindow.py
+++ redesign/OpenGLWindow.py
@@ -459,9 +459,9 @@ class OpenGLWindow(fltk.Fl_Gl_Window):
         self.redraw()
         
         fltk.Fl_check()
-        # self.redraw()
+        self.redraw()
         
-        # fltk.Fl_check()
+        fltk.Fl_check()
         
         # change backdrop image to binary
         # fltk.Fl_wait(0.5)
@@ -493,9 +493,9 @@ class OpenGLWindow(fltk.Fl_Gl_Window):
         
         self.redraw()
         fltk.Fl_check()
-        # self.redraw()
+        self.redraw()
         
-        # fltk.Fl_check()
+        fltk.Fl_check()
         # fltk.Fl_wait(0.5)
         # self.snap("test",save=True)
 
