6be5310b08d63d3d251d0f4cee573cce38ac9673
nymph <nymph@yannix.com>
2020-11-26 10:56:41 +0700

    draw new base selectMode

--- redesign/drawFunc.py
+++ redesign/drawFunc.py
@@ -306,6 +306,11 @@ def drawHex(ratio,selectedMode):
         for i in range(6):
             GL.glVertex2d(ratio*math.sin(i/6.0*2*math.pi),ratio*math.cos(i/6.0*2*math.pi))
         GL.glEnd()
+        inner = 0.3
+        outer = 4
+        GL.glRotatef(30,0,0,1)
+        GLUT.glutSolidTorus(inner,outer,6,6)
+        GL.glRotatef(-30,0,0,1)
     GL.glDisable(GL.GL_BLEND)
     GL.glPopAttrib()
 
